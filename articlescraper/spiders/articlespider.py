b36c4e5b06e9b554b6273fa23b28e66795197ce0
juliocesardemoraes <jcmcf2012@gmail.com>
2022-06-08 19:15:18 -0300

    fix: change object structure and implementing true upsert

--- articlescraper/spiders/articlespider.py
+++ articlescraper/spiders/articlespider.py
@@ -1,16 +1,50 @@
 import re
+import os
 import scrapy
-from textblob.sentiments import NaiveBayesAnalyzer
+from pymongo import UpdateOne
 from articlescraper.database import Database
-from textblob import Blobber
-from textblob.taggers import NLTKTagger
-import os;
 
+class Article():
+    """
+    Object that witholds an bbc article content
+
+    Parameters:
+        title (str): Article title
+        link (str): Article that redirects to article page
+        category (str): Article Category(Ex: Europe, USA, Culture)
+
+    Returns:
+        An Article Class Instance
+    """
+    def __init__(self, title, link, category) -> None:
+        self.title = title
+        self.link = link
+        self.category = category
+
+    def __getitem__(self, key):
+        return getattr(self, str(key))
+    def __setitem__(self, key, value):
+        return setattr(self, str(key), value)
+
+    def get_list(self):
+        """
+        Method for fetching the instance properties and values as a dict
+        so it can be read and inserted into the database
+
+        Parameters:
+            self
+
+        Returns:
+            Returns the instance class properties formatted as a json
+        """
+        return {"title":self.title, "link": self.link, "category": self.category}
 
 class ArticleSpider(scrapy.Spider):
     """
-    Main class to scrape web for articles
-    :param scrapy.Spider class: Responsible for crawling the website
+    Class used for scraping web for news articles, in this case bbc one
+
+    Parameters
+        scrapy.Spider (class): Responsible for crawling the website
     """
     name = 'article'
     start_urls = ['http://www.bbc.com/']
@@ -20,48 +54,48 @@ class ArticleSpider(scrapy.Spider):
     def parse(self, response):
         mongo = Database("bbc_" + os.getenv('DEPLOY_ENVIROMENT'))
 
-        article_title_list = []
-        article_link_list = []
-        article_theme_list = []
-
         index = 0
+
+        article_object = []
 
         for article in response.css('div.media__content'):
             article_to_format = article.css('a.media__link::text').get()
             article_link = article.css('a.media__link::attr(href)').get()
 
-            if index<5 :
-                if not re.search("https://", article_link):
-                    article_link_list.append("https://www.bbc.com" + article_link)
-                else:
-                    article_link_list.append(article_link)
-                article_title_list.append(article_to_format.strip())
 
+            if index<5 :
+                article_object.append(Article("","",""))
+
+                if not re.search("https://", article_link):
+                    article_object[index].link = ("https://www.bbc.com" + article_link)
+                else:
+                    article_object[index].link = article_link
+
+                article_object[index].title = article_to_format.strip()
                 theme = article.css('a.media__tag::text').get()
-                article_theme_list.append(theme)
+                article_object[index].category = theme
 
             index = index + 1
 
-        mongo.database_instance.replace_one({"article_title":{"$in": article_title_list}}, {"article_title": article_title_list,
-        "article_link": article_link_list,
-        "article_theme": article_theme_list }, upsert = True )
+        operations = []
+
+        for operation in article_object:
+            operations.append(UpdateOne({ "title": operation.title},{"$set": operation.get_list()},upsert=True))
+
+        mongo.database_instance.bulk_write(operations)
 
         return{
             "code": 200,
-            "objectToDatabase": {
-                "article_title": article_title_list,
-                "article_link": article_link_list,
-                "article_theme": article_theme_list
-            }
         }
 
-
+"""
+from textblob import Blobber
+from textblob.taggers import NLTKTagger
+from textblob.sentiments import NaiveBayesAnalyzer
         # NPL TO IMPLEMENT
         # tb = Blobber(analyzer=NaiveBayesAnalyzer())
         # nkl = Blobber(pos_tagger=NLTKTagger())
 
-
-"""
         for article in article_title_list:
             print("ARTICLE", article)
             blob = tb(article)
