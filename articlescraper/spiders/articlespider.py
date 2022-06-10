4f41ab4fda1d887b04d4a92619a022744fac42a5
juliocesardemoraes <jcmcf2012@gmail.com>
2022-06-10 15:09:14 -0300

    feat: add content and date variables and refactoring
    in this commit it was implemented two new variables to the crawler article class, and because of that some refactoring was necessary. Mainly it was necessary because to get the content you have to access the article page.,

--- articlescraper/spiders/articlespider.py
+++ articlescraper/spiders/articlespider.py
@@ -1,25 +1,32 @@
 import re
 import os
+import datetime
 import scrapy
-from pymongo import UpdateOne
 from articlescraper.database import Database
 
 class Article():
     """
-    Object that witholds an bbc article content
+    Object that witholds an article structure
 
     Parameters:
-        title (str): Article title
-        link (str): Article that redirects to article page
-        category (str): Article Category(Ex: Europe, USA, Culture)
+        category (str): Article Category(Ex: Europe, USA, Culture).
+        content (Array(str)): Article actual content.
+        date: Article Release Date.
+        image_link (str): Article image link to render.
+        link (str): Article that redirects to article page.
+        title (str): Article title.
 
     Returns:
         An Article Class Instance
     """
-    def __init__(self, title, link, category) -> None:
-        self.title = title
-        self.link = link
+    def __init__(self, category, content, date, image_link, link, title) -> None:
         self.category = category
+        self.content = content
+        self.date = date
+        self.image_link = image_link
+        self.link = link
+        self.title = title
+
 
     def __getitem__(self, key):
         return getattr(self, str(key))
@@ -37,14 +44,24 @@ class Article():
         Returns:
             Returns the instance class properties formatted as a json
         """
-        return {"title":self.title, "link": self.link, "category": self.category}
+        return {"title":self.title,
+                "link": self.link,
+                "category": self.category,
+                "image_link": self.image_link,
+                "content": self.content,
+                "date": self.date
+                }
 
 class ArticleSpider(scrapy.Spider):
     """
-    Class used for scraping web for news articles, in this case bbc one
+    Class used for scraping web for news articles,
+    in this case we're using the bbc news website
 
     Parameters
         scrapy.Spider (class): Responsible for crawling the website
+
+    Returns:
+        code 200 when it's successful
     """
     name = 'article'
     start_urls = ['http://www.bbc.com/']
@@ -52,19 +69,17 @@ class ArticleSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        mongo = Database("bbc_" + os.getenv('DEPLOY_ENVIROMENT'))
 
         index = 0
-
         article_object = []
 
         for article in response.css('div.media__content'):
             article_to_format = article.css('a.media__link::text').get()
             article_link = article.css('a.media__link::attr(href)').get()
 
-
+            # Checking index to only scrape the main articles
             if index<5 :
-                article_object.append(Article("","",""))
+                article_object.append(Article("","","","",[],""))
 
                 if not re.search("https://", article_link):
                     article_object[index].link = ("https://www.bbc.com" + article_link)
@@ -72,36 +87,47 @@ class ArticleSpider(scrapy.Spider):
                     article_object[index].link = article_link
 
                 article_object[index].title = article_to_format.strip()
+                
                 theme = article.css('a.media__tag::text').get()
                 article_object[index].category = theme
 
+                image = response.css('div.responsive-image')
+                image_src = image[index].css('img::attr(src)').get()
+
+                article_object[index].image_link = image_src
+
+                
+                article_time_fetch = datetime.datetime(2018, 6, 1)
+
+                article_object[index].date = article_time_fetch.strftime("%c")
+
             index = index + 1
 
-        operations = []
-
-        for operation in article_object:
-            operations.append(UpdateOne({ "title": operation.title},{"$set": operation.get_list()},upsert=True))
-
-        mongo.database_instance.bulk_write(operations)
+        for article in article_object:
+            yield scrapy.Request(article.link, callback=self.article_crawler, meta={'object': article})
 
         return{
             "code": 200,
         }
 
-"""
-from textblob import Blobber
-from textblob.taggers import NLTKTagger
-from textblob.sentiments import NaiveBayesAnalyzer
-        # NPL TO IMPLEMENT
-        # tb = Blobber(analyzer=NaiveBayesAnalyzer())
-        # nkl = Blobber(pos_tagger=NLTKTagger())
+    def article_crawler(self,response):
+        """
+        Method that enters each article page and fetches the content
+        and also inserts the object into the database
 
-        for article in article_title_list:
-            print("ARTICLE", article)
-            blob = tb(article)
-            blob2 = nkl(article)
-            print("TEXTO", blob)
-            print("BLAB", blob.sentiment)
-            print("TEXTO2", blob2)
-            print("BLAB2", blob2.sentiment)
-"""
+        Parameters:
+            self
+            response (Dict): An status code regarding the crawling
+
+        Returns:
+            No Return
+        """
+        mongo = Database("bbc_" + os.getenv('DEPLOY_ENVIROMENT'))
+        article_object = response.meta['object']
+
+        for article in response.css('article'):
+            content = article.css('p::text').extract()
+            article_object.content = content
+
+        mongo.database_instance.replace_one({ "title": article_object.title},
+        article_object.get_list(), upsert=True)
